9c4028e22071e2a31cd7353f2ab4a0b0b5e62ebb
Nathaniel Risum <nathaniel.risum2@gmail.com>
2024-04-08 16:23:49 +0200

    Add exceptions

---
+++ Z-Lib/Exceptions/IllegalNameException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z_Lib.Exceptions
+{
+    public class IllegalNameException : ArgumentNullException
+    {
+        public IllegalNameException() : base ("Empty or null name supplied, please enter a name"){}
+
+        public override string Message => base.Message;
+    }
+}
---
+++ Z-Lib/Exceptions/WrongCredentialsException.cs
@@ -0,0 +1,11 @@
+﻿namespace Z_Marked.Exceptions
+{
+    public class WrongCredentialsException : ArgumentException
+    {
+        public WrongCredentialsException() : base("Wrong login credentials. Try again.")
+        {}
+
+        public override string Message => base.Message;
+    }
+
+}
