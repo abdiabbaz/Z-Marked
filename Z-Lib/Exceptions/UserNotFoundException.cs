2d53b21da846e46d5f51b555208028210163b593
Nathaniel Risum <nathaniel.risum2@gmail.com>
2024-04-11 17:05:35 +0200

    Add Exceptions

---
+++ Z-Lib/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z_Lib.Exceptions
+{
+    public class UserNotFoundException : ArgumentException
+    {
+        public UserNotFoundException(string username, string password): base($"Ingen bruger fundet med brugernavn: {username} og kodeord: {password}"){ }
+        public UserNotFoundException(int id) : base($"Ingen bruger fundet med ID: {id}") { }
+        public UserNotFoundException(string username) : base($"Ingen bruger fundet med brugernavn: {username}") { }
+        public override string Message => base.Message;
+    }
+}
