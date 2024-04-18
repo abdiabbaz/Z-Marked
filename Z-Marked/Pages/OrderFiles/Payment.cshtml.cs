a7aab127a81e20e63666baa2c135f681283d294c
Marco Dybig <mad006@edu.zealand.dk>
2024-04-18 21:50:21 +0200

    Payment side lavet

---
+++ Z-Marked/Pages/OrderFiles/Payment.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Z_Marked.Pages.OrderFiles
+{
+    public class PaymentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
