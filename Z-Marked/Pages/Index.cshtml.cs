283542a39cb32a2957b93495f7fbeae8a86945e3
Abdirahman Abbas Sheikh Husein <abs001@edu.zealand.dk>
2024-04-18 23:53:48 +0200

    Late night working

--- Z-Marked/Pages/Index.cshtml.cs
+++ Z-Marked/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Z_Lib.Model;
 using Z_Marked.Model;
 using Z_Marked.Services;
 
@@ -18,7 +19,11 @@ namespace Z_Marked.Pages
         public IUserSource Repo;
         public List<Item> Items { get; set; }
 
+        public List<ItemCategory.Category> Categories { get; set; }
 
+        public double Total {  get; set; }  
+
+        public Dictionary<Item, int> ItemsPerPage { get; set; } 
         public IndexModel(ILogger<IndexModel> logger, IUserSource repo, IItemSource itemRepo, Order order)
         {
             _logger = logger;
@@ -40,9 +45,41 @@ namespace Z_Marked.Pages
 
         }
 
+        public IActionResult OnPostReset()
+        {
+            Items = _itemRepo.GetItems();
+            Categories = Categories = Enum.GetValues<ItemCategory.Category>().ToList();
+            return RedirectToPage(); 
+        }
+
+        public void OnPostSort(string category)
+        {
+
+            if (string.IsNullOrEmpty(category))
+            {
+                Items = _itemRepo.GetItems();
+            }
+            else
+            {
+                Items = _itemRepo.GetItems().Where(item => item.Category == category).ToList();
+                ItemsPerPage = _order.GetItemsDictionary();   
+            }
+            Categories = Enum.GetValues<ItemCategory.Category>().ToList();
+        }
+
         public void OnGet()
         {
             Items = _itemRepo.GetItems();
+            if (Categories == null)
+            {
+                Categories = new List<ItemCategory.Category>();
+            }
+            Categories = Categories = Enum.GetValues<ItemCategory.Category>().ToList();
+
+            ItemsPerPage = _order.GetItemsDictionary();
+
+            Total = _order.SumOfAllItemsDic();
+
         }
     }
 }
